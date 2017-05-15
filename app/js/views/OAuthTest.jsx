d05f550770e16d41355ef64481428243c094a3d3
Dan Goldin <dgoldin@triplelift.com>
2017-05-14 23:14:36 -0400

    Clean up styles

--- app/js/views/OAuthTest.jsx
+++ app/js/views/OAuthTest.jsx
@@ -6,25 +6,22 @@ export default class OAuthTest extends Component {
       <div>
         <h1>OAuth Test</h1>
 
-        <div>
+        <div className='section'>
             This will create a jsonify.me profile for you based on your LinkedIn or Twitter profile.
         </div>
 
         <hr />
 
-        <div>
-            Login to jsonify.me first. Note that you will need to register via the API. Check out the docs on the <a href='/'>home page</a> to get started.
+        <div className='section'>
+            Login to jsonify.me first. Note that you will need to register via the API.
+            Check out the docs on the <a href='/'>home page</a> to get started.
 
             <form>
-                <div>
-                    <label>Username</label>
-                    <input type="text" id="id_username" placeholder="username" name="username" />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" id="id_password" placeholder="password" name="password" />
-                </div>
-                <button type="submit">Login</button>
+              <div>
+                <input type='text' id='id_username' placeholder='username' name='username' />
+                <input type='password' id='id_password' placeholder='password' name='password' />
+              </div>
+              <button type='submit'>Login</button>
             </form>
         </div>
       </div>
