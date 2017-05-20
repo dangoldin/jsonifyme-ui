163fbe27454fe1af77fc8f60843b8981540d308c
Dan Goldin <dgoldin@triplelift.com>
2017-05-19 23:06:36 -0400

    Start working on login

--- app/js/views/OAuthTest.jsx
+++ app/js/views/OAuthTest.jsx
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
 
 export default class OAuthTest extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleLogin(e) {
+    e.preventDefault();
+
+    // Make request to authenticate and get token
+  }
+
+  handleChange(e) {
+    const field = e.target.name;
+    const state = this.state;
+    state[field] = e.target.value;
+
+    this.setState(state);
+  }
+
   render() {
     return (
       <div>
@@ -16,10 +42,22 @@ export default class OAuthTest extends Component {
             Login to jsonify.me first. Note that you will need to register via the API.
             Check out the docs on the <a href='/'>home page</a> to get started.
 
-            <form>
+            <form onSubmit={ this.handleLogin }>
               <div>
-                <input type='text' id='id_username' placeholder='username' name='username' />
-                <input type='password' id='id_password' placeholder='password' name='password' />
+                <input
+                  type='text'
+                  id='id_username'
+                  placeholder='username'
+                  name='username'
+                  onChange={ this.handleChange }
+                />
+                <input
+                  type='password'
+                  id='id_password'
+                  placeholder='password'
+                  name='password'
+                  onChange={ this.handleChange }
+                />
               </div>
               <button type='submit'>Login</button>
             </form>
